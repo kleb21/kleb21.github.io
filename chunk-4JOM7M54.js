4e47cf370d672f8af5ef27743045018d11a0c30e
Caleb <caleb.mmacha@gmail.com>
2026-03-31 18:44:04 -0600

    Auto-generated commit

---
+++ chunk-4JOM7M54.js
@@ -0,0 +1,1 @@
+import"./chunk-2NFLSA4Y.js";var t=[{path:"",loadComponent:()=>import("./chunk-XDNVAK5S.js").then(o=>o.ProjectListPageComponent)}],e=t;export{e as default};
---
+++ chunk-GSLDLLHJ.js
@@ -0,0 +1,1 @@
+import"./chunk-2NFLSA4Y.js";var t=[{path:"",loadComponent:()=>import("./chunk-IF76EJCM.js").then(o=>o.HomePageComponent)}],e=t;export{e as default};
